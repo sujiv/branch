00e83ebce71b00dbef5af6f1b1f321f2bc8787ac
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2020-04-08 00:37:51 -0400

    initial commit

---
+++ src/main/java/com/altimetrik/ocrbatch/OcrbatchApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.ocrbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altimetrik/ocrbatch/OcrbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.ocrbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
