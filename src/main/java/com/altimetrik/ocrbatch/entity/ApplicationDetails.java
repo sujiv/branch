29c53b565097747fec46327dde85dad06dab48b7
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2020-04-09 12:19:45 -0400

    User Input entity updated

--- src/main/java/com/altimetrik/ocrbatch/entity/ApplicationDetails.java
+++ src/main/java/com/altimetrik/ocrbatch/entity/ApplicationDetails.java
@@ -36,8 +36,24 @@ public class ApplicationDetails {
     private Double EDIL_ObtainedFrmJan31ToBeRefinanced;    //Double?
     private Double PPP_LoadAmntLesserOfCalcOr10Mil;    //Double?
     private String fieldComments;   //Contains JSON as string
-    private String fieldAutoVerified;   //Contains JSON as string
-    private String applicationComments;
+    private String fieldAutoVerified = "{\"FTE_Emp12MnthsPrior\":\"N\",\"empWages\":\"N\",\"lessOwnerWagesExcess100K\":\"N\",\"lessQualifiedSickLeaveWagesUnderFFCRA\":\"N\",\"lessQualifiedFamilyLeaveWagesUnderFFCRA\":\"N\",\"groupHealthCareBenefitsInsPremium\":\"N\",\"paymentRetirementBen\":\"N\",\"paymentEmployerPayrollTaxesStateLocal\":\"N\",\"contractLabor\":\"N\",\"lessIndividualContractLaborExcess100K\":\"N\",\"prior12MnthsCumQualifyingPayrollCost\":\"N\",\"avgMonthlyPayrollcosts\":\"N\",\"multiplier2dot5\":\"N\",\"EDIL_ObtainedFrmJan31ToBeRefinanced\":\"N\",\"PPP_LoadAmntLesserOfCalcOr10Mil\":\"N\"}\n";
+    private String applicationComments = "{\n" +
+            "    \"FTE_Emp12MnthsPrior\": \"\",\n" +
+            "    \"empWages\": \"\",\n" +
+            "    \"lessOwnerWagesExcess100K\": \"\",\n" +
+            "    \"lessQualifiedSickLeaveWagesUnderFFCRA\": \"\",\n" +
+            "    \"lessQualifiedFamilyLeaveWagesUnderFFCRA\": \"\",\n" +
+            "    \"groupHealthCareBenefitsInsPremium\": \"\",\n" +
+            "    \"paymentRetirementBen\": \"\",\n" +
+            "    \"paymentEmployerPayrollTaxesStateLocal\": \"\",\n" +
+            "    \"contractLabor\": \"\",\n" +
+            "    \"lessIndividualContractLaborExcess100K\": \"\",\n" +
+            "    \"prior12MnthsCumQualifyingPayrollCost\": \"\",\n" +
+            "    \"avgMonthlyPayrollcosts\": \"\",\n" +
+            "    \"multiplier2dot5\": \"\",\n" +
+            "    \"EDIL_ObtainedFrmJan31ToBeRefinanced\": \"\",\n" +
+            "    \"PPP_LoadAmntLesserOfCalcOr10Mil\": \"\"\n" +
+            "}";
     private Boolean processedFlag = false;
     private String processStatus;
     private Timestamp createdTs;
