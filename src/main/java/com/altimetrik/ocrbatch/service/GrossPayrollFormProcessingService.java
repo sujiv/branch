0bc54c3b2869f09240e502ed89e95796eb39ce24
OgabekYuldashov <ogabek.yuldashov@gmail.com>
2020-04-09 14:47:43 -0400

    appDetails coments and autverified JSON fields have been populated; try/catch bocks introduced

--- src/main/java/com/altimetrik/ocrbatch/service/GrossPayrollFormProcessingService.java
+++ src/main/java/com/altimetrik/ocrbatch/service/GrossPayrollFormProcessingService.java
@@ -6,20 +6,16 @@ import com.altimetrik.ocrbatch.repository.FileStorageRepository;
 import com.altimetrik.ocrbatch.utils.Utils;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
-import java.util.List;
-import java.util.Locale;
 
 @Service
 public class GrossPayrollFormProcessingService {
@@ -30,7 +26,10 @@ public class GrossPayrollFormProcessingService {
     @Autowired
     private Tesseract tesseract;
 
-    public ApplicationDetails processGrossPayroll(ApplicationDetails appDetails, FileStorage fileStorage) throws IOException, TesseractException, ParseException {
+    public ApplicationDetails processGrossPayroll(ApplicationDetails appDetails, FileStorage fileStorage) throws IOException, TesseractException {
+
+        JSONObject appCommentsObj = new JSONObject(appDetails.getApplicationComments());
+        JSONObject appAutoVerifiedObj = new JSONObject(appDetails.getFieldAutoVerified());
 
         BufferedImage bufferedImage = Utils.createImageFromBytes(fileStorage.getGrossPayroll());
         String result = tesseract.doOCR(bufferedImage);
@@ -44,59 +43,90 @@ public class GrossPayrollFormProcessingService {
         String sbaGrossPay = lineWords[1];
         NumberFormat format = NumberFormat.getCurrencyInstance();
 
-        DecimalFormat nf = (DecimalFormat) NumberFormat.getInstance(Locale.GERMANY);
-        nf.setParseBigDecimal(true);
-        BigDecimal bd = (BigDecimal) nf.parse("2,6");
-
         System.out.println("sbaGrossPay " + sbaGrossPay);
-        Number number1 = format.parse(sbaGrossPay);
-        System.out.println("sbaGrossPay : " +number1.toString());
-//        appDetails.set
-
+        Number number1 = null;
+        try {
+            number1 = format.parse(sbaGrossPay);
+            System.out.println("sbaGrossPay : " +number1.toString());
+            //TODO: set SBS GROSS PAY VALUE to appDetails
+        } catch (ParseException e) {
+            e.printStackTrace();
+            //TODO: set SBS GROSS PAY exception comment to appDetails
+        }
 
         String empStateLocalTaxes = lineWords[2];
-//        NumberFormat format = NumberFormat.getCurrencyInstance();
-        Number number = format.parse(empStateLocalTaxes);
+        Number number = null;
+        try {
+            number = format.parse(empStateLocalTaxes);
+            System.out.println("empStateLocalTaxes : " +number.toString());
 
-        System.out.println("empStateLocalTaxes : " +number.toString());
+            appDetails.setPaymentEmployerPayrollTaxesStateLocal(Double.valueOf(number.toString()));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            appCommentsObj.put("paymentEmployerPayrollTaxesStateLocal", e.getMessage());
 
-        appDetails.setPaymentEmployerPayrollTaxesStateLocal(Double.valueOf(number.toString()));
+        }
 
-        String empBenifts = lineWords[4].replaceAll("[^0-9]", "");
-        System.out.println("empBenifts : " +empBenifts);
-        appDetails.setPaymentRetirementBen(Double.valueOf(empBenifts));
+        try {
+            String empBenifts = lineWords[4].replaceAll("[^0-9]", "");
+            System.out.println("empBenifts : " +empBenifts);
+            appDetails.setPaymentRetirementBen(Double.valueOf(empBenifts));
+            appAutoVerifiedObj.put("paymentRetirementBen", "Y");
+        }catch (Exception e) {
+            e.printStackTrace();
+            appCommentsObj.put("paymentRetirementBen", e.getMessage());
+        }
 
-        String payrollCost = lineWords[5].replaceAll("[^0-9]", "");
-        System.out.println("payrollCost : " +payrollCost);
-        appDetails.setPrior12MnthsCumQualifyingPayrollCost(Double.valueOf(payrollCost));
-
-        String AvgTotals = lines[29];
-        System.out.println(AvgTotals);
-        String[] AvgTotalWords = AvgTotals.split(" ");
-        String AvgVal = AvgTotalWords[1];
-        Number number3 = format.parse(AvgVal);
-        System.out.println("AvgVal : " +number3);
-        appDetails.setAvgMonthlyPayrollcosts(Double.valueOf(number3.toString()));
-        //for restricting to 2 decimals
-        Double valueForCal2dot5 = appDetails.getAvgMonthlyPayrollcosts()* 2.5d;
-        DecimalFormat df = new DecimalFormat("#.##");
-        valueForCal2dot5 = Double.valueOf(df.format(valueForCal2dot5));
-        appDetails.setMultiplier2dot5(valueForCal2dot5);
+        try {
+            String payrollCost = lineWords[5].replaceAll("[^0-9]", "");
+            System.out.println("payrollCost : " +payrollCost);
+            appDetails.setPrior12MnthsCumQualifyingPayrollCost(Double.valueOf(payrollCost));
+            appAutoVerifiedObj.put("prior12MnthsCumQualifyingPayrollCost", "Y");
+        }catch (Exception e) {
+            e.printStackTrace();
+            appCommentsObj.put("prior12MnthsCumQualifyingPayrollCost", e.getMessage());
+        }
 
 
-        if(appDetails.getMultiplier2dot5() > 10000000.00)
-            appDetails.setPPP_LoadAmntLesserOfCalcOr10Mil(10000000.00);
-        else
-            appDetails.setPPP_LoadAmntLesserOfCalcOr10Mil(appDetails.getMultiplier2dot5());
+        try {
+            String AvgTotals = lines[29];
+            System.out.println(AvgTotals);
+            String[] AvgTotalWords = AvgTotals.split(" ");
+            String AvgVal = AvgTotalWords[1];
+            Number number3 = null;
+            number3 = format.parse(AvgVal);
+            System.out.println("AvgVal : " +number3);
+            appDetails.setAvgMonthlyPayrollcosts(Double.valueOf(number3.toString()));
+            appAutoVerifiedObj.put("paymentEmployerPayrollTaxesStateLocal", "Y");
+        } catch (Exception e) {
+            e.printStackTrace();
+            appCommentsObj.put("paymentEmployerPayrollTaxesStateLocal", e.getMessage());
+        }
 
-        //setting all the empty value to zero
-        appDetails.setLessIndividualContractLaborExcess100K(0.0);
-        appDetails.setLessOwnerWagesExcess100K(0.0);
-        appDetails.setLessQualifiedSickLeaveWagesUnderFFCRA(0.0);
-        appDetails.setLessQualifiedFamilyLeaveWagesUnderFFCRA(0.0);
-        appDetails.setGroupHealthCareBenefitsInsPremium(0.0);
-        appDetails.setContractLabor(0.0);
-        appDetails.setEDIL_ObtainedFrmJan31ToBeRefinanced(0.0);
+        try {
+            //for restricting to 2 decimals
+            Double valueForCal2dot5 = appDetails.getAvgMonthlyPayrollcosts()* 2.5d;
+            DecimalFormat df = new DecimalFormat("#.##");
+            valueForCal2dot5 = Double.valueOf(df.format(valueForCal2dot5));
+            appDetails.setMultiplier2dot5(valueForCal2dot5);
+
+            appAutoVerifiedObj.put("multiplier2dot5", "Y");
+        }catch (Exception e) {
+            e.printStackTrace();
+            appCommentsObj.put("multiplier2dot5", e.getMessage());
+        }
+
+        try {
+            if(appDetails.getMultiplier2dot5() > 10000000.00)
+                appDetails.setPPP_LoadAmntLesserOfCalcOr10Mil(10000000.00);
+            else
+                appDetails.setPPP_LoadAmntLesserOfCalcOr10Mil(appDetails.getMultiplier2dot5());
+
+            appAutoVerifiedObj.put("PPP_LoadAmntLesserOfCalcOr10Mil", "Y");
+        }catch (Exception e) {
+            e.printStackTrace();
+            appCommentsObj.put("PPP_LoadAmntLesserOfCalcOr10Mil", e.getMessage());
+        }
 
         return appDetails;
     }
